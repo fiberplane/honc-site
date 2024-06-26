12d846b8281097355dcc6269995ad0b41b38d44b
Oscar <33732524+oscarvz@users.noreply.github.com>
2024-06-26 16:15:46 +0200

    Add basic static assets (#2)
    * Initialize project
    
    * Specify package manager
    
    * Add static fonts & update config
    
    * Add `Inter` font to preload tags

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,11 +1,29 @@
 import { jsxRenderer } from "hono/jsx-renderer";
+import { Link } from "honox/server";
 
-export const renderer = jsxRenderer(({ children, title }) => {
+export default jsxRenderer(({ children, title }) => {
   return (
     <html lang="en">
       <head>
         <title>{title ?? "HONC.dev"}</title>
+
+        <Link href="/app/style.css" rel="stylesheet" />
+        <link
+          rel="preload"
+          href="/static/fonts/Silkscreen-Bold.woff2"
+          as="font"
+          type="font/woff2"
+          crossorigin=""
+        />
+        <link
+          rel="preload"
+          href="/static/fonts/Inter.woff2"
+          as="font"
+          type="font/woff2"
+          crossorigin=""
+        />
       </head>
+
       <body>{children}</body>
     </html>
   );
