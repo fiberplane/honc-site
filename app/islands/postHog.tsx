b95fde0b55fae3f474b5654588580596f3fe7c55
Oscar <33732524+oscarvz@users.noreply.github.com>
2024-07-26 15:50:53 +0200

    Update env var import (#10)

--- app/islands/postHog.tsx
+++ app/islands/postHog.tsx
@@ -1,14 +1,21 @@
 import { useEffect } from "hono/jsx";
 import posthog from "posthog-js";
 
-const posthogProjectKey = import.meta.env.VITE_POSTHOG_PROJECT_KEY;
-
 /**
  * HACK: Honox's client.ts only gets initialised only when an island exists;
  * therefore we need to create an island to initialise PostHog.
  */
 export default function PostHog() {
   useEffect(() => {
+    const posthogProjectKey =
+      import.meta.env.VITE_POSTHOG_PROJECT_KEY ||
+      process.env.VITE_POSTHOG_PROJECT_KEY;
+
+    if (!posthogProjectKey) {
+      console.error("PostHog project key not set");
+      return;
+    }
+
     posthog.init(posthogProjectKey, {
       api_host: "https://us.i.posthog.com",
       person_profiles: "identified_only",
