c7ad52c21bdde69717df224892f41a2fe73672d5
oscarvz <oscar@fiberplane.com>
2024-07-26 16:44:00 +0200

    Pass key as prop to island component

--- app/islands/postHog.tsx
+++ app/islands/postHog.tsx
@@ -1,26 +1,26 @@
-import { useEffect } from "hono/jsx";
+import { Fragment, useEffect } from "hono/jsx";
 import posthog from "posthog-js";
+
+type PostHogProps = {
+  projectKey?: string;
+};
 
 /**
  * HACK: Honox's client.ts only gets initialised only when an island exists;
  * therefore we need to create an island to initialise PostHog.
  */
-export default function PostHog() {
+export default function PostHog({ projectKey }: PostHogProps) {
   useEffect(() => {
-    const posthogProjectKey =
-      import.meta.env.VITE_POSTHOG_PROJECT_KEY ||
-      process.env.VITE_POSTHOG_PROJECT_KEY;
-
-    if (!posthogProjectKey) {
-      console.error("PostHog project key not set");
+    if (!projectKey) {
+      console.error("PostHog project key is not defined.");
       return;
     }
 
-    posthog.init(posthogProjectKey, {
+    posthog.init(projectKey, {
       api_host: "https://us.i.posthog.com",
       person_profiles: "identified_only",
     });
-  }, []);
+  }, [projectKey]);
 
-  return <></>;
+  return <Fragment />;
 }
