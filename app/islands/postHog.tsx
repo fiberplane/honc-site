5cf7874b96cd138f53fd9737ff429cfde93db7a1
Oscar <33732524+oscarvz@users.noreply.github.com>
2024-07-26 12:48:55 +0200

    Add Posthog analytics (#6)
    * Add client-side Posthog
    
    * Fix env variable
    
    * Add `.env.example`

---
+++ app/islands/postHog.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "hono/jsx";
+import posthog from "posthog-js";
+
+const posthogProjectKey = import.meta.env.VITE_POSTHOG_PROJECT_KEY;
+
+/**
+ * HACK: Honox's client.ts only gets initialised only when an island exists;
+ * therefore we need to create an island to initialise PostHog.
+ */
+export default function PostHog() {
+  useEffect(() => {
+    posthog.init(posthogProjectKey, {
+      api_host: "https://us.i.posthog.com",
+      person_profiles: "identified_only",
+    });
+  }, []);
+
+  return <></>;
+}
