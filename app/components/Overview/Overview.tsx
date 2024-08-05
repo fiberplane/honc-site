156971a962f9887339849e356a260f631f7879f9
oscarvz <oscar@fiberplane.com>
2024-08-05 23:10:22 +0200

    Sequenced animations tryout

--- app/components/Overview/Overview.tsx
+++ app/components/Overview/Overview.tsx
@@ -14,11 +14,81 @@ export function Overview({ children, title }: OverviewProps) {
       <section class={sectionClass}>
         <h1 id={anchorIds.overview}>{title}</h1>
 
-        <ul>{children}</ul>
+        <div class={stackContainer}>
+          <div class={overflowContainer} />
+          <ul>{children}</ul>
+        </div>
       </section>
     </Wrapper>
   );
 }
+
+const cardAnimation = keyframes`
+  to {
+    translate: 0 0;
+  }
+`;
+
+const sectionClass = css`
+  --overview-gap: 1px;
+  --overview-radius: 2rem;
+  --overview-animation-duration: 0.5s;
+  --overview-animation-delay: 0.3s;
+
+  h1 {
+    text-align: center;
+    font-size: clamp(2.5rem, 14lvw, 5rem);
+  }
+
+  ul {
+    border-radius: var(--overview-radius);
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: 1fr;
+    gap: var(--overview-gap);
+    list-style: none;
+    padding: 0;
+
+    & > li {
+      padding: 3rem 2rem;
+      width: 100%;
+      background-color: var(--color-bg-elevated);
+      animation-name: ${cardAnimation};
+      animation-duration: var(--overview-animation-duration);
+      animation-fill-mode: forwards;
+      animation-timing-function: ease-in;
+
+      &:nth-child(1) {
+        translate: -1rem -1rem;
+        border-top-left-radius: var(--overview-radius);
+      }
+
+      &:nth-child(2) {
+        translate: 1rem -1rem;
+        border-top-right-radius: var(--overview-radius);
+        animation-delay: var(--overview-animation-delay);
+      }
+
+      &:nth-child(3) {
+        translate: -1rem 1rem;
+        border-bottom-left-radius: var(--overview-radius);
+        animation-delay: calc(var(--overview-animation-delay) * 2);
+      }
+
+      &:nth-child(4) {
+        translate: 1rem 1rem;
+        border-bottom-right-radius: var(--overview-radius);
+        animation-delay: calc(var(--overview-animation-delay) * 3);
+      }
+    }
+  }
+`;
+
+const stackContainer = css`
+  position: relative;
+  isolation: isolate;
+  padding: var(--overview-gap);
+`;
 
 const scaleAnimation = keyframes`
   to {
@@ -28,72 +98,44 @@ const scaleAnimation = keyframes`
 
 const shadowAnimation = keyframes`
   to {
-    box-shadow: 0 0 2rem 1.75rem rgb(from var(--color-bg-primary) r g b / 0.1);
+    box-shadow: 0 0 2rem 1.75rem rgb(from var(--color-bg-primary) r g b / 0.15);
   }
 `;
 
-const sectionClass = css`
-  h1 {
-    text-align: center;
-    font-size: clamp(2.5rem, 14lvw, 5rem);
-  }
+const overflowContainer = css`
+  --overview-scale-animation-duration: 0.7s;
 
-  ul {
-    --gap: 1px;
-    --radius: 2rem;
+  --overview-scale-animation-delay: calc(
+      calc(var(--overview-animation-duration) * 3)
+  );
 
-    border-radius: var(--radius);
-    overflow: hidden;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-auto-rows: 1fr;
-    gap: var(--gap);
-    list-style: none;
-    padding: 0;
-    padding: var(--gap);
-    position: relative;
-    animation: ${shadowAnimation} 0.7s forwards ease-out 1s;
+  --overview-shadow-animation-delay: calc(
+    var(--overview-scale-animation-delay)
+  );
 
-    &::before {
-      content: "";
-      display: block;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      z-index: -1;
-      padding-top: 100%;
-      background: var(--color-bg-primary);
-      border-radius: 99999px;
-      translate: -50% -50%;
-      transform-origin: center;
-      width: 120%;
-      aspect-ratio: 1;
-      scale: 0;
-      outline: none;
+  position: absolute;
+  inset: 0;
+  overflow: hidden;
+  border-radius: 2rem;
+  animation: ${shadowAnimation} 0.7s forwards ease-out
+    var(--overview-shadow-animation-delay);
 
-      animation: ${scaleAnimation} 0.7s forwards ease-out .7s;
-    }
-
-    & > li {
-      padding: 3rem 2rem;
-      width: 100%;
-      background-color: var(--color-bg-elevated);
-
-      &:nth-child(1) {
-        border-top-left-radius: var(--radius);
-      }
-
-      &:nth-child(2) {
-        border-top-right-radius: var(--radius);
-      }
-
-      &:nth-child(3) {
-        border-bottom-left-radius: var(--radius);
-      }
-
-      &:nth-child(4) {
-        border-bottom-right-radius: var(--radius);
-      }
-    }
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    z-index: -1;
+    padding-top: 100%;
+    background: var(--color-bg-primary);
+    border-radius: 99999px;
+    translate: -50% -50%;
+    transform-origin: center;
+    width: 120%;
+    aspect-ratio: 1;
+    scale: 0;
+    animation: ${scaleAnimation} var(--overview-scale-animation-duration)
+      forwards ease-out var(--overview-scale-animation-delay);
   }
 `;
