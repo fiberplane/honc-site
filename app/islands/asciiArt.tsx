d3894ddda05a3bda1497461a957ed2052054ad2e
oscarvz <oscar@fiberplane.com>
2024-08-05 21:47:20 +0200

    Prevent layout shift by trimming initial ASCII render

--- app/islands/asciiArt.tsx
+++ app/islands/asciiArt.tsx
@@ -66,7 +66,7 @@ export default function AsciiArt() {
 
   return (
     <pre ref={preRef} class={preClass}>
-      <span>{asciiArt}</span>
+      <span>{asciiArt.trim()}</span>
     </pre>
   );
 }
