226d301e3bae07c98e80fec26e76ea1acfcfd54f
oscarvz <oscar@fiberplane.com>
2024-08-06 12:32:40 +0200

    Trim ASCII art

--- app/islands/asciiArt.tsx
+++ app/islands/asciiArt.tsx
@@ -66,7 +66,7 @@ export default function AsciiArt() {
 
   return (
     <pre ref={preRef} class={preClass}>
-      <span>{asciiArt.trim()}</span>
+      <span>{asciiArt}</span>
     </pre>
   );
 }
