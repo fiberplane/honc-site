0bd2baab6caf844f6a93611f104c643c8f9f3b6f
oscarvz <oscar@fiberplane.com>
2024-08-06 14:14:34 +0200

    Introduce get started component & split hero

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./Footer";
 export * from "./Hero";
 export * from "./icons";
+export * from "./Intro";
 export * from "./Nav";
 export * from "./Overview";
 export * from "./Quickstart";
