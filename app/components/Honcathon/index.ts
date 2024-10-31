411a570db03d2b850a633c69748239c2dac91236
oscarvz <oscar@fiberplane.com>
2024-10-31 13:59:18 +0100

    Group sections

--- app/components/Honcathon/index.ts
+++ app/components/Honcathon/index.ts
@@ -1,5 +1,6 @@
 export * from "./HoncathonAsciiLogo";
 export * from "./HoncathonContentBlock";
 export * from "./HoncathonHeader";
+export * from "./HoncathonSection";
 export * from "./HoncathonSponsors";
 export * from "./SignUpForm";
