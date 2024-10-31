8f9c99b0348b638852bd2a9eb7e2ce09bb3e8b54
oscarvz <oscar@fiberplane.com>
2024-10-31 14:35:01 +0100

    Update prizes list

--- app/components/Honcathon/index.ts
+++ app/components/Honcathon/index.ts
@@ -1,6 +1,7 @@
 export * from "./HoncathonAsciiLogo";
 export * from "./HoncathonContentBlock";
 export * from "./HoncathonHeader";
+export * from "./HoncathonPrizesList";
 export * from "./HoncathonSection";
 export * from "./HoncathonSponsors";
 export * from "./SignUpForm";
