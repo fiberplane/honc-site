b0f153567763c161496a6415190b3864eb68d2c7
oscarvz <oscar@fiberplane.com>
2024-12-10 15:57:15 +0100

    Update Honcathon prize list

--- app/components/Honcathon/HoncathonPrizesList.tsx
+++ app/components/Honcathon/HoncathonPrizesList.tsx
@@ -12,27 +12,60 @@ export function HoncathonPrizesList({ children }: PropsWithChildren) {
 }
 
 const wrapperClassName = css`
-  ul {
+  & > ul {
     list-style: none;
     margin: 0;
     padding: 0;
     display: grid;
     gap: 0.5rem;
 
-    li {
+    & > li {
       background-color: var(--color-bg-elevated);
       padding: 3rem;
       border-radius: 30px;
       padding-inline-start: 6em;
       position: relative;
-      
-      &::before{
+
+      ul {
+        list-style: none;
+        padding: 0;
+
+        li {
+          padding-inline: 3rem;
+          position: relative;
+
+          &::before {
+            content: "🪿";
+            position: absolute;
+            left: 1rem;
+          }
+        }
+      }
+
+      &::before {
         content: "🏅";
         font-size: 3em;
         position: absolute;
         left: 1.75rem;
         top: 50%;
         translate: 0 -50%;
+      }
+
+      &:first-of-type {
+        padding-inline-start: 3rem;
+        padding-block-start: 180px;
+        text-align: center;
+        top: 0;
+        font-size: 1.6em;
+        border: 1px solid var(--color-fg-primary);
+
+        &::before {
+          content: "🏆";
+          font-size: 5rem;
+          left: 50%;
+          top: 0;
+          translate: -50% 5rem;
+        }
       }
     }
   }
