f15a535e36f6c8f26424312f92608fcd50831c3f
Oscar <33732524+oscarvz@users.noreply.github.com>
2025-03-11 17:45:32 +0100

    Upgrade dependencies & transition to Workers (#49)
    * Upgrade dependencies
    
    * Move from Cloudflare Pages to Workers
    
    * Update project name
    
    * Fix SVG inline loading in `OverviewItem`
    
    * Add recommended VS Code extensions
    
    * Add custom domain to Wrangler config
    
    * Update GitHub action & deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import build from "@hono/vite-cloudflare-pages";
-import ssg from "@hono/vite-ssg";
+import build from "@hono/vite-build/cloudflare-workers";
+import adapter from "@hono/vite-dev-server/cloudflare";
 import mdx from "@mdx-js/rollup";
 import honox from "honox/vite";
 import rehypeExternalLinks, { type Options } from "rehype-external-links";
@@ -7,33 +7,22 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { defineConfig } from "vite";
 
-const entry = "./app/server.ts";
-
 const rehypeExternalLinksOptions: Options = {
   target: "_blank",
   rel: ["noopener", "noreferrer"],
 };
 
 export default defineConfig({
-  build: {
-    assetsDir: "static",
-    ssrEmitAssets: true,
-    target: "esnext",
-  },
   plugins: [
     honox({
-      client: {
-        assetsDir: "static",
-        jsxImportSource: "hono/jsx",
-        input: ["/app/style.css"],
-      },
+      devServer: { adapter },
+      client: { input: ["./app/style.css", "./app/honacthon.css"] },
     }),
-    build(),
-    ssg({ entry }),
     mdx({
       jsxImportSource: "hono/jsx",
       rehypePlugins: [[rehypeExternalLinks, rehypeExternalLinksOptions]],
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     }),
+    build(),
   ],
 });
