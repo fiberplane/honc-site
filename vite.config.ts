1e872ff9d6f96ecaa837981da021ae62d8d6c77c
oscarvz <oscar@fiberplane.com>
2025-03-11 13:42:09 +0100

    Upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import build from "@hono/vite-cloudflare-pages";
-import ssg from "@hono/vite-ssg";
+import adapter from "@hono/vite-dev-server/cloudflare";
 import mdx from "@mdx-js/rollup";
 import honox from "honox/vite";
 import rehypeExternalLinks, { type Options } from "rehype-external-links";
@@ -7,29 +7,15 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { defineConfig } from "vite";
 
-const entry = "./app/server.ts";
-
 const rehypeExternalLinksOptions: Options = {
   target: "_blank",
   rel: ["noopener", "noreferrer"],
 };
 
 export default defineConfig({
-  build: {
-    assetsDir: "static",
-    ssrEmitAssets: true,
-    target: "esnext",
-  },
   plugins: [
-    honox({
-      client: {
-        assetsDir: "static",
-        jsxImportSource: "hono/jsx",
-        input: ["/app/style.css"],
-      },
-    }),
+    honox({ devServer: { adapter } }),
     build(),
-    ssg({ entry }),
     mdx({
       jsxImportSource: "hono/jsx",
       rehypePlugins: [[rehypeExternalLinks, rehypeExternalLinksOptions]],
